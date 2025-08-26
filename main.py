75894af4754a5395289aef6caa2977bbdf3640c7
pumpk1n986 <office@tom-haderer.com>
2025-08-26 13:38:13 +0200

    CH2 L2 done

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    clock = pygame.time.Clock()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Quit event received")
+                running = False  # this breaks the while loop
+
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
