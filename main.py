ed684f70aa18a70d2a00deb0f262456ff9f5c531
pumpk1n986 <office@tom-haderer.com>
2025-08-26 12:14:14 +0200

    CH2 L1 done

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
