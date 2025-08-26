a1e569aa6987127d953679fefe51eb6fb90ca094
pumpk1n986 <office@tom-haderer.com>
2025-08-26 15:01:02 +0200

    CH2 L5 done

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     running = True
     clock = pygame.time.Clock()
+    dt = 0
 
     while running:
         for event in pygame.event.get():
@@ -14,6 +17,7 @@ def main():
                 running = False  # this breaks the while loop
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
 
