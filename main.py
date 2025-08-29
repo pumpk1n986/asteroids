6690c1659a6ad5fcfaf0c204efb6e19e09e9a07b
pumpk1n986 <office@tom-haderer.com>
2025-08-29 19:55:46 +0200

    CH3 L1 done

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     running = True
     clock = pygame.time.Clock()
-    dt = 0
 
     while running:
+        dt = clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("Quit event received")
                 running = False  # this breaks the while loop
 
+        updatable.update(dt / 1000)
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
 
     pygame.quit()
 
